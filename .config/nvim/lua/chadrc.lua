996cfa6c2754104937d2d6ad1271ec87ff987a72
Arif Khan <153022036+bytepacket@users.noreply.github.com>
2025-04-09 08:13:40 -0400

    Update chadrc.lua
    redid (shamelessly copied) code

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,16 +1,90 @@
--- This file needs to have same structure as nvconfig.lua 
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
----@type ChadrcConfig
-local M = {}
+local options = {
 
-M.base46 = {
-	theme = "nightowl",
-
-	hl_override = {
-	 	Comment = { italic = true },
-	 	["@comment"] = { italic = true },
-	 },
-}
-
-return M
+	base46 = {
+	  theme = "nightowl", -- default theme
+	  hl_add = {},
+	  hl_override = {},
+	  integrations = {},
+	  changed_themes = {},
+	  transparency = false,
+	},
+  
+	ui = {
+	  cmp = {
+		icons = true,
+		lspkind_text = true,
+		style = "atom_colored", -- default/flat_light/flat_dark/atom/atom_colored
+	  },
+  
+	  telescope = { style = "borderless" }, -- borderless / bordered
+  
+	  statusline = {
+		theme = "default", -- default/vscode/vscode_colored/minimal
+		-- default/round/block/arrow separators work only for default statusline theme
+		-- round and block will work for minimal theme only
+		separator_style = "default",
+		order = nil,
+		modules = nil,
+	  },
+  
+	  -- lazyload it when there are 1+ buffers
+	  tabufline = {
+		enabled = true,
+		lazyload = true,
+		order = { "treeOffset", "buffers", "tabs", "btns" },
+		modules = nil,
+	  },
+  
+	  nvdash = {
+		load_on_startup = true,
+  
+		header = {
+		  "           ▄ ▄                   ",
+		  "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+		  "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+		  "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+		  "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+		  "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+		  "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+		  "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+		  "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+		},
+  
+		buttons = {
+		  { "  Find File", "␣ f f", "Telescope find_files" },
+		  { "󰈚  Recent Files", "␣ f o", "Telescope oldfiles" },
+		  { "󰈭  Find Word", "␣ f w", "Telescope live_grep" },
+		  { "  Bookmarks", "␣ m a", "Telescope marks" },
+		  { "  Themes", "␣ t h", "Telescope themes" },
+		  { "  Mappings", "␣ c h", "NvCheatsheet" },
+		},
+	  },
+	},
+  
+	term = {
+	  winopts = { number = false, relativenumber = false },
+	  sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+	  float = {
+		relative = "editor",
+		row = 0.3,
+		col = 0.25,
+		width = 0.5,
+		height = 0.4,
+		border = "single",
+	  },
+	},
+  
+	lsp = { signature = true },
+  
+	cheatsheet = {
+	  theme = "grid", -- simple/grid
+	  excluded_groups = { "terminal (t)", "autopairs", "Nvim", "Opens" }, -- can add group name or with mode
+	},
+  
+	mason = { cmd = true, pkgs = {} },
+  }
+  
+  local status, chadrc = pcall(require, "chadrc")
+  return vim.tbl_deep_extend("force", options, status and chadrc or {})
